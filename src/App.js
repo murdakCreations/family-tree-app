a5236f1e3d75cbd972a8a26a41d3aa8cd5e0a621
Marie Curie Robiego <marie.robiego@goabroad.com>
2024-09-05 10:39:27 +0800

    displayed data in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import { getDocs, query, collection } from 'firebase/firestore';
 import { db } from './firestore';
+import { useEffect, useState } from 'react';
 
-const q = query(collection(db, "hamsters"));
-    await getDocs(q)
-        .then((querySnapshot)=>{               
-          const newData = querySnapshot.docs
-            .map((doc) => ({...doc.data()}));
-          console.log(newData)           
-        })
 
-function App() {
+const App = () => {
+  const [hamsters, setHamsters] = useState([])
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async() => {
+    const q = query(collection(db, "hamsters"));
+      await getDocs(q)
+          .then((querySnapshot)=>{               
+            const newData = querySnapshot.docs
+              .map((doc) => ({...doc.data()}));
+
+            console.log(newData)
+            newData.forEach(data => {
+              setHamsters(
+                [
+                  ...hamsters,
+                  {
+                    name: data.name,
+                    gender: data.gender,
+                    birthdate: data.birth_date,
+                  }
+                ]
+              );
+            });
+            
+          });
+  }
+  
   return (
     <>
-      yo
+      {
+        hamsters.map(hamster => {
+          return (
+            <>
+              <div>{hamster.name}</div>
+              <div>{hamster.gender}</div>
+              <div>{hamster.birthdate}</div>
+            </>
+          )
+        })
+      }
     </>
   );
 }
